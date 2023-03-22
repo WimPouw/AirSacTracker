2df6b6ebe7270a4702220de9354491a87525ee3e
Wim Pouw <wimpouw@gmail.com>
2023-03-22 17:26:12 +0100

    corrected_proof_of_concept

--- Proof_of_concept_analyses/STEP_1_annotation_tosnippets_2.py
+++ Proof_of_concept_analyses/STEP_1_annotation_tosnippets_2.py
@@ -19,6 +19,7 @@ it = 0
 
 andata = np.array(pandas.read_csv(toprocess)) #save into an numpy array (but first read.csv using pandas)     
 for ch in range(0, andata.shape[0]): #now loop through each row of your annotions
+    print(ch)
     annot = andata[ch,0]
     ID = annot.replace('_zoom_synced_boom', '')
     session = ID.split('Session',1)[1][1]
@@ -29,16 +30,16 @@ for ch in range(0, andata.shape[0]): #now loop through each row of your annotion
     endtime = andata[ch,2] #extract end time
     annotlabel = str(andata[ch,3]) + '_' + str(andata[ch,4])  + '_' + str(andata[ch,5])#extract your annotation
     #now take the video and select the relevant chunk and write to the outputfolder
-    ffmpeg_extract_subclip(videof+video[0], begintime/1000, (endtime/1000), targetname=outputfol+"_"+ID+ "_" +str(ch)+"_" +str(annotlabel)+vidformat)
+    ffmpeg_extract_subclip(videof+video[0], begintime/1000, (endtime/1000), targetname=outputfol+"_"+ID+ "_"+str(ch)+"_" +str(annotlabel)+vidformat)
     #exract boom
     audioboomf = processed_data+'/'+date+'_processed/Session' + session + '/boom_mic/' #get the video that has a matching code
     audioboom = os.listdir(audioboomf)
     myaudio = AudioSegment.from_file(audioboomf+audioboom[0])
     snipped = myaudio[begintime:endtime]
-    snipped.export(outputfol2+"_boommic_"+ID+ "_" + str(ch) +"_" +str(annotlabel)+".wav", format='wav')
+    snipped.export(outputfol2+"_boommic_"+ID+"_" +str(annotlabel)+".wav", format='wav')
     #exract gopros combined
     audiof = processed_data+'/'+date+'_processed/Session' + session + '/gopros_mics/' #get the video that has a matching code
     audiogo= os.listdir(audiof)
     myaudio = AudioSegment.from_file(audiof+audiogo[0])
     snipped = myaudio[begintime:endtime]
-    snipped.export(outputfol2+"_multisource_"+ID+ "_" + str(ch) +"_" +str(annotlabel)+".wav", format='wav')
+    snipped.export(outputfol2+"_multisource_"+ID +"_" +str(annotlabel)+".wav", format='wav')
